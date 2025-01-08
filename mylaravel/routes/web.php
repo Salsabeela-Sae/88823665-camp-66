1a458d6a969df999ce0e60f4752c2024d1f015bc
Salsabeela-Sae <salsabeelasae@gmail.com>
2025-01-08 20:23:50 +0700

    myview

--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyController;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', function () {
-    return "<h1> Hello World!</h1>";
-});
+Route::get('/mycontroller', [MyController::class, 'myfunction']);
+Route::post('/mycontroller', [MyController::class, 'myfunction']);
