c008e2bc9178ec9a2c52e02dde2d6dc221c4a5a2
Salsabeela-Sae <salsabeelasae@gmail.com>
2025-01-29 11:33:09 +0700

    template laravel

---
+++ mylaravel/resources/views/registers.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="register-box">
+    <div class="register-logo">
+      <a href="../index2.html"><b>Admin</b>LTE</a>
+    </div>
+    <div class="card">
+      <div class="card-body register-card-body">
+        <p class="login-box-msg">Register a new membership</p>
+        <form action="../index3.html" method="post">
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Full name" />
+            <div class="input-group-text"><span class="bi bi-person"></span></div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="email" class="form-control" placeholder="Email" />
+            <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Password" />
+            <div class="input-group-text"><span class="bi bi-lock"></span></div>
+          </div>
+          <div class="input-group mb-3">
+            <input type="password" class="form-control" placeholder="Retype password" />
+            <div class="input-group-text"><span class="bi bi-lock"></span></div>
+          </div>
+          <div class="row">
+            <div class="col-8">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="termsCheck" />
+                <label class="form-check-label" for="termsCheck"> I agree to the terms </label>
+              </div>
+            </div>
+            <div class="col-4">
+              <div class="d-grid gap-2">
+                <button type="submit" class="btn btn-primary">Register</button>
+              </div>
+            </div>
+          </div>
+        </form>
+        <p class="mb-0">
+          <a href="login" class="text-center"> I already have a membership </a>
+        </p>
+      </div>
+    </div>
+</div>
+@endsection
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -2,6 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MyController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegisterController;
 
-Route::get('/mycontroller', [MyController::class, 'myfunction']);
-Route::post('/mycontroller', [MyController::class, 'myfunction']);
+//ระบุ part url ตัว route
+Route::get('/home',
+    [HomeController::class, 'home']);
+
+Route::get('/',
+    [HomeController::class, 'home']);
+
+Route::get('/login',
+    [LoginController::class, 'index']);
+
+Route::get('/registers' ,
+    [RegisterController::class, 'index']);
+
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/hello', function () {
+    return "<h1>Hello World!</h1>";
+});
+
+Route::get("/mycontroller/{id?}",
+    [MyController::class, 'myfunction']);
+
+Route::post("/mycontroller/{id?}",
+    [MyController::class, 'myfunction']);
+
+Route::get('/500', function () {
+        abort(500);
+    });
+
+Route::get('/404', function () {
+        abort(404);
+    });
+
